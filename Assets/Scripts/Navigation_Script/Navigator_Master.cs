377d21fc18a7f10dd49c501801c086a962f74a58
Vitor shiraishi <shumbinhodota@hotmail.com>
2019-06-09 18:46:40 -0300

    Inicio de navegação, pastas e Menu inicial

---
+++ Assets/Scripts/Navigation_Script/Navigator_Master.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigator_Master : MonoBehaviour
+{
+    GameObject nextScreen;
+    GameObject currentScreen;
+    GameObject[] screens;
+
+    // Start is called before the first frame update
+    public void ChangeScreen(string nextScreen){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
