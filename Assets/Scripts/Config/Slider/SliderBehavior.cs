197a4c34b26f1f5d34b74a999ed336df423d2301
Lauro Lobo <laurofilipe@gmail.com>
2019-06-16 18:27:28 -0300

    Adicionado nova Tela e configuração de slider.

---
+++ Assets/Scripts/Config/Slider/SliderBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderBehavior : MonoBehaviour
+{
+    public float Som = 0f;
+    public float Musica = 0f;
+    public Slider musica;
+    public Slider som;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMusic();
+    }
+    public void GetMusic()
+    {
+        Musica = musica.value;
+         Som = som.value;
+    }
+}
