d87df72425f4dd99426f2bfae2ab824dfa1adb15
Rodrigo Nakamuta <nakamuta26@gmail.com>
2019-06-16 18:49:58 -0300

    Criação de script de eventos(frente e verso da "carta")

---
+++ Assets/Materials/Eventos/Scripts/mechanicaEvento.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mechanicaEvento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Image card;
+    public GameObject Objeto;
+    void Start()
+    {
+       card = gameObject.GetComponent<Image>();
+        desliga();
+    }
+    public void evento_flip()
+    {
+
+    }
+    void liga()
+    {
+        Objeto.SetActive(true);
+        card.enabled = false;
+    }
+    void desliga()
+    {
+        Objeto.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
