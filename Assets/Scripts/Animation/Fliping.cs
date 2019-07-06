6c6ad282ee3e272b3a3a70b0f312eb28f930e678
Rodrigo Nakamuta <nakamuta26@gmai.com>
2019-07-06 16:26:33 -0300

    Animções de evento completa

---
+++ Assets/Scripts/Animation/Fliping.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fliping : MonoBehaviour
+{
+    public Animator Flip;
+    public Vector2 temp;
+
+    public bool liberageral2 = false;
+    public bool liberageral = false; 
+    public bool libera = false; // libera para o inicio da animação das escolhas
+    public float tempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Flip = gameObject.GetComponent<Animator>();
+    }
+    public void flipping()
+    {
+        if (Flip.GetCurrentAnimatorStateInfo(0).IsName("Event-flip"))           
+        {
+            Flip.SetBool("Finished", true);
+            libera = true;
+        }
+    }
+    public void temporal()// para medir o tempo das animações
+    {
+        if (libera == true || liberageral2 == true)
+        {
+            tempo += Time.deltaTime;
+        }
+        if (tempo > 1 && liberageral2 == false)
+        {
+            libera = false;
+            liberageral = true;
+            tempo = 0;
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        temporal();
+    }
+}
---
+++ Assets/Scripts/Animation/Flipingverso.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipingverso : MonoBehaviour
+{
+    public Fliping carta; // script do mainimage 
+    public Animator verso; // animação das "choices"
+    public bool played = false; // pra verificar se já rodou a ultima animação
+    // Start is called before the first frame update
+    void Start()
+    {
+        verso = gameObject.GetComponent<Animator>();
+    }
+    void anima()// função que atua encima da animação do objetos "Choice" ida
+    {
+        verso.SetBool("Descent", true);
+        if(carta.liberageral2 == true && carta.tempo >= 1 && played == false)
+        {
+            verso.SetBool("Ascent", true);
+            verso.Play("Flip-choices-back");
+            played = true;
+        }
+    }
+    public void anima2()//função que atua encima da animação do objetos "Choice" volta
+    {
+        if (played == false)
+        {
+            verso.SetBool("Ascent", true);
+            verso.Play("Flip-choices-back");
+            carta.liberageral2 = true;
+            played = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(carta.liberageral == true)
+        {
+            anima();
+        }
+    }
+}
