d144adbf71594e7ead40875c7cbca6cf3ae2d46d
Enio Moura <enio.moura@supera.com.br>
2019-07-13 18:36:41 -0300

    Corrigindo script yes/no

--- Assets/Scripts/Menu_Items/Confirm.cs
+++ Assets/Scripts/Menu_Items/Confirm.cs
@@ -4,31 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Confirm : MonoBehaviour
 {
-    public bool exists = false;
-    public GameObject yesno; //o objeto ao qual precisa confirmar alguma ação
-    public Image placeholder;
-    public Button botao;
-    public string nomeobjeto; // nome do objeto botão que irá ter precisar de alguma confirmação 
-    // Start is called before the first frame update
-    void Start()
-    {
-        yesno = GameObject.Find(nomeobjeto);
-        placeholder = gameObject.GetComponent<Image>();
-        botao = gameObject.GetComponent<Button>();
-        placeholder.enabled = false;
-        botao.enabled = false;
-    }
+    public bool exists;
+    public GameObject[] yesno; //o objeto ao qual precisa confirmar alguma ação
     public void confirmed()
     {
-        if (yesno.GetComponent<navegacao>() && yesno.GetComponent<navegacao>().yes == true)
+        if (!exists)
         {
-            placeholder.enabled = true;
-            botao.enabled = true;
+            foreach (GameObject g in yesno)
+            {
+                g.SetActive(true);
+            }
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (exists)
+        {
+            foreach (GameObject g in yesno)
+            {
+                g.SetActive(false);
+            }
+        }
     }
 }
