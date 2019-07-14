a050c51e2496ff140a1cbc8b31a69d14a86257b4
Rodrigo Nakamuta <nakamuta26@gmail.com>
2019-07-14 15:11:48 -0300

    Mexendo em ajustes de câmera

---
+++ Assets/Scripts/cameraad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraad : MonoBehaviour {
+	public float aspect;
+    public RectTransform tela;
+    public float altura;
+    public float largura;
+	// Use this for initialization
+	void Awake () {
+        Debug.Log(Screen.height);
+        Debug.Log(Screen.height * Camera.main.aspect);
+        //Camera.main.aspect = aspect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
